35112371b019d0192244d5809abe818adb172498
afnando <afnanhasnuddin99@gmail.com>
2022-06-26 00:33:08 +0800

    add navbar to dashboard and make custom textfield

---
+++ lib/views/complaint/dashboard_view.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:tm_mobile_app/constants/routes.dart';
+import 'package:tm_mobile_app/enums/menu_action.dart';
+import 'package:tm_mobile_app/services/auth/auth_service.dart';
+import 'package:tm_mobile_app/views/navigation_bar.dart';
+
+class DashboardView extends StatefulWidget {
+  const DashboardView({Key? key}) : super(key: key);
+
+  @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text(
+          'Dashboard',
+          style: TextStyle(
+            // color: Colors.black,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await AuthService.firebase().logOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      loginRoute,
+                      (_) => false,
+                    );
+                  }
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text("Logout"),
+                ),
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Text('Hello world'),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sign Out'),
+        content: const Text('Are you sure you want to sign?'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Cancel')),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Log Out')),
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
+}
---
+++ lib/views/complaint/widget/custom_text_input.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+// reusable textformfield widget
+Widget makeInput({label, controller, obsureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        label,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+      ),
+      const SizedBox(height: 7),
+      TextFormField(
+          controller: controller,
+          minLines: 1,
+          maxLines: 5,
+          style: const TextStyle(fontSize: 20),
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.red,
+                width: 5,
+              ),
+            ),
+            prefixIcon: Icon(Icons.format_list_bulleted_rounded),
+          ),
+          onSaved: (String? val) {
+            controller = val;
+          })
+    ],
+  );
+}
