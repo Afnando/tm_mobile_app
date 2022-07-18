048d678f1c0994425d3e906497165916e6223ec6
afnando <afnanhasnuddin99@gmail.com>
2022-07-18 14:10:00 +0800

    uncomment

--- lib/services/cloud/cloud_storage_constans.dart
+++ lib/services/cloud/cloud_storage_constans.dart
@@ -1,12 +1,13 @@
-// //define cloud
-// const idField = 'id';
+//define cloud
+const idField = 'id';
 // const ownerUserIdField = 'ownerUserId';
-// const logDateField = 'logDate';
-// // const monthField = 'month';
-// // const stakeholderField = 'stakeholder';
-// // const picField = 'pic';
-// const issuesField = 'issues';
-// const actionFIeld = 'action';
-// const rfsiField = 'rfsi';
-// // const ownerField = 'owner';
-// const dateClosedField = 'dateClosed';
+const logDateField = 'logDate';
+// const monthField = 'month';
+const stakeholderField = 'stakeholder';
+const picField = 'pic';
+const issuesField = 'issues';
+const actionFIeld = 'action';
+const rfsiField = 'rfsi';
+const ownerField = 'owner';
+const statusField = 'status';
+const dateClosedField = 'dateClosed';
